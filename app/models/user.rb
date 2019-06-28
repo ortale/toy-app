f7bc5a1805931bce548691b162f83bd93db4cb49
Jose Ortale <ortale22@gmail.com>
2019-06-28 09:05:24 +0100

    presence removed from models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ApplicationRecord
     has_many :microposts
-
-    validates :content, presence: true    # Replace FILL_IN with the right code.
-    validates :user_id, presence: true    # Replace FILL_IN with the right code.
 end
